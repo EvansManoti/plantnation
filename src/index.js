595ae74a52949ba22081e5beb83e79759394d289
Evans <manotievance@gmail.com>
2023-10-18 09:20:00 +0300

    completed assignmentclear

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+let page = 1;
+let paginationElement = document.getElementById("pagination");
+let paginationbtns=paginationElement.getElementsByTagName("button")
+//console.log(paginationbtns)
+
+
+paginationbtns[0].addEventListener('click', back);
+function back(){
+    console.log("back")
+    if (page == 1){
+        return;
+    }
+    page = page -1;
+}
